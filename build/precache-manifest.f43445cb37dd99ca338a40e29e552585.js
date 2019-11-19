96bd9a8d48cd584490e0452c08421e236734f6dc
Ponchimeow <trancetoradora@gmail.com>
2019-11-19 12:27:16 +0800

    1119

---
+++ build/precache-manifest.f43445cb37dd99ca338a40e29e552585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb4481cc4167348eaca3be644bfb9ed",
+    "url": "/Gomuku/index.html"
+  },
+  {
+    "revision": "1b13d9e48bba709412d0",
+    "url": "/Gomuku/static/css/main.9e81fa91.chunk.css"
+  },
+  {
+    "revision": "c6f7bb200e2729d788c9",
+    "url": "/Gomuku/static/js/2.6918dc63.chunk.js"
+  },
+  {
+    "revision": "1b13d9e48bba709412d0",
+    "url": "/Gomuku/static/js/main.1bce972b.chunk.js"
+  },
+  {
+    "revision": "6c0910af9aaa9d3c705c",
+    "url": "/Gomuku/static/js/runtime-main.b8c1110b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bd308336d1ecfd7c5bd4f26e2170146.js"
+  "/Gomuku/precache-manifest.f43445cb37dd99ca338a40e29e552585.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Gomuku/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
